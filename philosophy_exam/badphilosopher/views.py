fd3d99a69a0f8263a8300c212184ef8420971f81
bd240897 <bd240897@yandex.ru>
2022-06-14 16:15:33 +0300

    add generic view

--- philosophy_exam/badphilosopher/views.py
+++ philosophy_exam/badphilosopher/views.py
@@ -4,25 +4,24 @@ from django.http import HttpResponse
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView
+
 from .models import Lections, Seminars
 
-
-class LectionsAnswerListView(View):
+class LectionsAnswerListView(ListView):
     """ Список вопросов """
+    model = Lections
+    paginate_by = 10
+    queryset = Lections.objects.order_by('number').all()
+    context_object_name = 'achieves_obj'
+    template_name = "badphilosopher/lections_answer_list.html"
 
-    def get(self, request, *args, **kwargs):
-        achieves = Lections.objects.order_by('number').all()
-        paginator = Paginator(achieves, 10)
-        page_number = request.GET.get('page')
-        achieves_obj  = paginator.get_page(page_number)
-        return render(request, 'badphilosopher/lections_answer_list.html', context={'achieves_obj': achieves_obj})
-
-class LectionsDetailedAnswerlView(View):
+class LectionsDetailedAnswerlView(DetailView):
     """ Страница одного ответа"""
-
-    def get(self, request, slug, *args, **kwargs):
-        answer = get_object_or_404(Lections, number=slug)
-        return render(request, 'badphilosopher/detailed_answer.html', context={'answer': answer})
+    model = Lections
+    slug_field = "number"
+    context_object_name = "answer"
+    template_name = 'badphilosopher/detailed_answer.html'
 
 class MainPagelView(View):
     """Главная страница"""
@@ -30,19 +29,17 @@ class MainPagelView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'badphilosopher/main.html')
 
-class SeminarsAnswerListView(View):
+class SeminarsAnswerListView(ListView):
     """ Семинары. Список ответов """
+    model = Seminars
+    paginate_by = 10
+    queryset = Seminars.objects.order_by('number').all()
+    context_object_name = 'achieves_obj'
+    template_name = "badphilosopher/seminars_answer_list.html"
 
-    def get(self, request, *args, **kwargs):
-        achieves = Seminars.objects.order_by('number').all()
-        paginator = Paginator(achieves, 10)
-        page_number = request.GET.get('page')
-        achieves_obj = paginator.get_page(page_number)
-        return render(request, 'badphilosopher/seminars_answer_list.html', context={'achieves_obj': achieves_obj})
-
-class SeminarsDetailedAnswerlView(View):
+class SeminarsDetailedAnswerlView(DetailView):
     """ Семинары. Страница одного ответа """
-
-    def get(self, request, slug, *args, **kwargs):
-        answer = get_object_or_404(Seminars, number=slug)
-        return render(request, 'badphilosopher/detailed_answer.html', context={'answer': answer})
+    model = Seminars
+    slug_field = "number"
+    context_object_name = "answer"
+    template_name = 'badphilosopher/detailed_answer.html'
