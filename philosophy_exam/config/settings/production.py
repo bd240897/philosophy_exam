053490ea12b217f375e99da6331b16961b0a0ec8
bd240897 <bd240897@yandex.ru>
2022-07-07 15:04:21 +0300

    add allowed hosts

--- philosophy_exam/config/settings/production.py
+++ philosophy_exam/config/settings/production.py
@@ -1,4 +1,4 @@
 from .base import *
 
 # хост
-ALLOWED_HOSTS = ['127.0.0.1', 'bad-philosopher.ru', 'www.bad-philosopher.ru']
+ALLOWED_HOSTS = ['127.0.0.1', 'bad-philosopher.ru', 'www.bad-philosopher.ru', "45.67.58.152"]
