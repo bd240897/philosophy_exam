ce6741fab52059a92aab1ff388f000bfc4a2da85
bd240897 <bd240897@yandex.ru>
2022-07-08 00:32:34 +0300

    add entrypoint v10

--- philosophy_exam/entrypoint.sh
+++ philosophy_exam/entrypoint.sh
@@ -8,7 +8,6 @@ python manage.py migrate
 if [ "$DJANGO_SUPERUSER_USERNAME" ]
 then
     python manage.py createsuperuser \
-        --noinput \
         --username $DJANGO_SUPERUSER_USERNAME \
         --email DJANGO_SUPERUSER_EMAIL
 fi
