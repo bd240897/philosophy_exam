7b1e343d910e461834d8200435c48959f5b54239
bd240897 <bd240897@yandex.ru>
2022-07-08 16:13:14 +0300

    add superuser try_2 v4

--- philosophy_exam/entrypoint.sh
+++ philosophy_exam/entrypoint.sh
@@ -13,4 +13,6 @@ python manage.py loaddata db.json
 #        --email $DJANGO_SUPERUSER_EMAIL
 #fi
 
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
+
 exec "$@"
