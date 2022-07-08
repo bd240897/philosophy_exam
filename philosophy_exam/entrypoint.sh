41871c89cc865b87b362f646a6e9ba5c57d4bafa
bd240897 <bd240897@yandex.ru>
2022-07-08 16:18:01 +0300

    add superuser FINAL

--- philosophy_exam/entrypoint.sh
+++ philosophy_exam/entrypoint.sh
@@ -6,13 +6,6 @@ python manage.py makemigrations contact
 python manage.py migrate
 python manage.py loaddata db.json
 
-#if [ "$DJANGO_SUPERUSER_USERNAME" ]
-#then
-#    python manage.py createsuperuser \
-#        --username $DJANGO_SUPERUSER_USERNAME \
-#        --email $DJANGO_SUPERUSER_EMAIL
-#fi
-
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')"
 
 exec "$@"
