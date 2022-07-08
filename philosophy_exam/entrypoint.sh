27bda03786671e09f08afe0b550ccb586e1c367c
bd240897 <bd240897@yandex.ru>
2022-07-08 15:44:07 +0300

    add load data

--- philosophy_exam/entrypoint.sh
+++ philosophy_exam/entrypoint.sh
@@ -4,6 +4,7 @@ python manage.py migrate
 python manage.py makemigrations badphilosopher
 python manage.py makemigrations contact
 python manage.py migrate
+python manage.py loaddata db.json
 
 if [ "$DJANGO_SUPERUSER_USERNAME" ]
 then
